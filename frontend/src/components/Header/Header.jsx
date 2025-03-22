4b5630a6837d5a53fe67d9ef7b354fded307be86
Nebil-Abdulfetah <159676747+Nebil-Abdulfetah@users.noreply.github.com>
2025-03-22 13:48:27 +0300

    Implemented logout button

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,14 +1,24 @@
 import React from "react";
+import axios from "axios";
 import logo from "../../assets/img/logo.png";
 import styles from "./header.module.css";
 import { IoMdMenu } from "react-icons/io";
-import Footer from "../Footer/Footer";
+import { CiLogout } from "react-icons/ci";
 function Header() {
+  async function handleLogout() {
+    await axios.post(
+      "http://localhost:5000/auth/logout",
+      {},
+      { withCredentials: true }
+    );
+    localStorage.clear();
+    window.location.href = "/login";
+  }
   return (
     <>
       <header>
         <div className={styles.logo_container}>
-          <img src={logo} alt="" className={styles.logo}/>
+          <img src={logo} alt="" className={styles.logo} />
         </div>
         <nav>
           <ul className={styles.nav_item_list}>
@@ -36,6 +46,14 @@ function Header() {
               <a href="" className={styles.nav_link}>
                 Employees
               </a>
+            </li>
+            <li>
+              <button className={styles.logout} onClick={handleLogout}>
+                <span>Logout</span>
+                <span>
+                  <CiLogout />
+                </span>
+              </button>
             </li>
           </ul>
         </nav>
